847c6a09b34f217610261bf9663f473a1c6b844d
Allen Chou <s3131212@gmail.com>
2019-02-11 19:35:57 +0800

    Initial commit ot gh-h-pages

---
+++ precache-manifest.d202bc7168cb8b67afcf204214a6f7b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae9594c7fb4571f2f139",
+    "url": "/static/js/main.ae9594c7.chunk.js"
+  },
+  {
+    "revision": "137d4b6a096a6bbb5db9",
+    "url": "/static/js/1.137d4b6a.chunk.js"
+  },
+  {
+    "revision": "ae9594c7fb4571f2f139",
+    "url": "/static/css/main.e7d46b0f.chunk.css"
+  },
+  {
+    "revision": "96bf04f9ba801dcc8103c3486ae9bb53",
+    "url": "/index.html"
+  }
+];
