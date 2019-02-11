5615a1dc83ebd28f8fa4497a50b02b2c25863994
Allen Chou <s3131212@gmail.com>
2019-02-12 01:19:45 +0800

    Better UX

--- src/App.js
+++ src/App.js
@@ -6,9 +6,30 @@ const LAW_NOT_FOUND = -1, CLAUSE_NOT_FOUND = -1, CLAUSE_NOT_SPECIFIED = -2;
 
 class LawContainer extends Component {
     render(){
+        if(this.props.name == '' && this.props.content == ''){
+            return (
+                <div className="ts heading centered slate attached">
+                    <span className="header lawName">使用方式</span>
+                    <span className="description lawContent">
+                        <p>舉例來說，<code>民法第 979-1 條</code>可以用以下方式查詢（中文與數字之間的空格可省略）：</p>
+                        <ul>
+                            <li>民法 979-1</li>
+                            <li>民 979-1</li>
+                            <li>民 979 1</li>
+                            <li>ㄇㄈ 979 1</li>
+                            <li>ㄇ 979 1</li>
+                        </ul>
+                        <p>其中 <code>ㄇㄈ</code> 是民（ㄇㄧㄣˊ）法（ㄈㄚˇ）的注音首字，若同個注音會對應多個法典（如刑法與憲法都是 ㄒㄈ），則會匹配到第一條。</p>
+                        <p>支援常見法典簡稱（如 <code>中華民國刑法 = 刑法</code>、<code>刑事訴訟法 = 刑訴</code>、<code>社會秩序維護法 = 社維法</code>）。</p>
+                        <p>若全名、注音、簡稱皆沒有找到匹配的法典，則會去尋找名稱最接近的法條，例如 <code>性平教</code> 會找到最接近的 <code>性別平等教育法</code>。</p>
+                    </span>
+                </div>
+            );
+        }
+
         return (
             <div className="ts heading centered slate attached">
-                <span className="header lawName">{this.props.name}</span>
+                <span className="header lawName">{this.props.name}　</span>
                 <span className="description lawContent">{this.props.content}</span>
             </div>
         );
@@ -89,11 +110,11 @@ class NextButton extends Component {
     render() {
         if(!this.props.available)
             return (
-                <div className="ts negative button disabled">下一條</div>
+                <div className="ts inverted button disabled">下一條</div>
             );
         else
             return (
-                <div className="ts negative button" onClick={ () =>
+                <div className="ts inverted button" onClick={ () =>
                     this.props.setClause(this.props.lawIndex, parseInt(this.props.clauseIndex) + 1)
                 }>下一條</div>
             );
@@ -138,8 +159,8 @@ class App extends Component {
         }
         if(clauseIndex == CLAUSE_NOT_FOUND){
             this.setState({
-                name: "",
-                content: lawName + "沒有此條",
+                name: lawName + "沒有此條",
+                content: "",
                 prevStatus: 0,
                 nextStatus: 0
             });
@@ -147,8 +168,8 @@ class App extends Component {
         }
         if(clauseIndex == CLAUSE_NOT_SPECIFIED){
             this.setState({
-                name: "",
-                content: "正在搜尋" + lawName,
+                name: "已選定：" + lawName,
+                content: "",
                 prevStatus: 0,
                 nextStatus: 0
             });
@@ -181,7 +202,7 @@ class App extends Component {
                 <div className="ts centered secondary">
                     <form className="ts form" onSubmit={e => { e.preventDefault(); }}>
                         <div className="field">
-                            <input type="text" placeholder="法律名稱" onChange={this.handleChange} value={this.state.value} />
+                            <input type="text" placeholder="法律名稱（如：刑法 35）" onChange={this.handleChange} value={this.state.value} />
                         </div>
                     </form>
                 </div>
